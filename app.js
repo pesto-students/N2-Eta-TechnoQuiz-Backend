81c16023dba007c1b844c6c4f8e296f25125936c
Divyanshu Verma <liitcode@gmail.com>
2021-05-23 14:15:17 +0530

    Update app.js
    Updated port for testing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.port || 3000;
 app.get('/',(req,res) => {
     res.send("It's not a good time to be here");
 })
-app.listen(3000, () => {
+
+app.listen(port, () => {
     console.log("Div Test"); 
 })
