9f7f126735f45d6a73e50444f7b6cac9d641c499
Divyanshu Verma <liitcode@gmail.com>
2021-05-24 15:39:18 +0530

    Master Branch

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+var http = require('http');
 const port = process.env.port || 3000;
 console.log("Grabbed Port",port)
-app.get('/',(req,res) => {
-    res.send("It's not a good time to be here");
-})
 
-app.listen(port, () => {
-    console.log("Div Test"); 
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
+  
+//   app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+//   })
+
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.write("Server Teting");
+    res.end();    
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
