1fa0db9c926cf75d0c5410ced8f52960904f66ef
Divyanshu Verma <liitcode@gmail.com>
2021-05-28 04:47:50 +0530

    TQ-20 #done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongo = require('mongoose');
 const dotenv = require('dotenv');
 //Import Routes
-const authRoute = require('./routes/auth');
+const authRoute = require('./routes/auth/auth');
+const leaderBoardRoute = require('./routes/leaderboard/leaderboard');
 dotenv.config();
 //Connect to DB
 mongo.connect(process.env.DB_CONNECT,{ useNewUrlParser: true,useUnifiedTopology: true },()=>console.log("Connected to DB"))
@@ -13,5 +14,6 @@ app.use(express.json());
 
 //Route Middelware 
 app.use('/api/user',authRoute);
+app.use('/api/leaderBoard',leaderBoardRoute);
 
 app.listen(3000,()=> console.log("Server up and running")); 
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../models/user');
+const User = require('../../models/user')
 const {registerValidation,loginValidation} = require('./validation');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
