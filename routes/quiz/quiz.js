fbdcaa8f94a3ba3aaa96056046928dc034fc8fd7
Divyanshu Verma <liitcode@gmail.com>
2021-06-01 10:38:30 +0530

    Deployment stage 1

--- routes/quiz/quiz.js
+++ routes/quiz/quiz.js
@@ -8,12 +8,12 @@ var validateReq = require('./validation')
 router.post('/', verifyUser,async (req,res) => {
     const { error } = validateReq(req.body);
     if(error) return res.status(400).send(error.details[0].message);
-    const apiUrl = process.env.API_BASE+category[req.body.category]+'&difficulty='+difficulty[req.body.difficulty]+'&limit=10';
+    const apiUrl = process.env.QAPI_BASE+category[req.body.category]+'&difficulty='+difficulty[req.body.difficulty]+'&limit=10';
     var data = {
         method: 'get',
         url: apiUrl ,
         headers: { 
-          'X-Api-Key': process.env.API_TOKEN
+          'X-Api-Key': process.env.QAPI_TOKEN
         }
       };
     await axios(data)
