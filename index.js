20c0e7def3dec52f23908ae21c0d22b7a5e11e31
Divyanshu Verma <69303193+liitcode@users.noreply.github.com>
2021-06-15 22:48:29 +0530

    Revert "Updated Conflicts (#1)" (#4)
    This reverts commit 83312ba52d5f13af98b7f582737ebcbe90153ea7.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+
 const app = express();
 const dotenv = require('dotenv');
+
 dotenv.config();
 require('./database/config');
+require('./routes/cron');
 
-//Import Routes
-const cron = require('./routes/cron');
+// Import Routes
 const authRoute = require('./routes/auth/auth');
 const leaderBoardRoute = require('./routes/leaderboard/leaderboard');
 const category = require('./routes/categories/categories');
@@ -13,18 +15,8 @@ const quiz = require('./routes/quiz/quiz');
 const score = require('./routes/score/score');
 const payment = require('./routes/payment/payment');
 
-//MiddleWare
+// MiddleWare
 app.use(express.json());
-
-//Route Middelware
-app.use('/api/user',authRoute);
-app.use('/api/leaderBoard',leaderBoardRoute);
-app.use('/api/category',category);
-app.use('/api/quiz',quiz);
-app.use('/api/score',score);
-app.use('/api/pay',payment);
-
-app.listen(3000,()=> console.log("Server up and running")); 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, Auth-Token, Content-Type, Accept');
@@ -40,5 +32,5 @@ app.use('/api/pay', payment);
 app.get('/', (req, res) => {
   res.status(200).send('TechnoQuiz Backend');
 });
-const port = process.env.port || 8000;
+const port = process.env.port || 3000;
 app.listen(port);
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,48 +1,48 @@
-const mongo = require('../../database/config');
+/* eslint-disable consistent-return */
+/* eslint-disable no-underscore-dangle */
+require('../../database/config');
 const router = require('express').Router();
-const User = require('../../models/user')
-const {registerValidation,loginValidation} = require('./validation');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const User = require('../../models/user');
+const { registerValidation, loginValidation } = require('./validation');
 
-router.post('/register',async (req,res) => {
-    const { error } = registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+router.post('/register', async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const emailExist = await User.findOne({email:req.body.email});
-    if(emailExist) return res.status(400).send('Email already exists');
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already exists');
 
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password,salt);
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-    const user = new User({
-        name : req.body.name,
-        email : req.body.email,
-        password : hashPassword
-    });
-    try{
-        const savedUser = await user.save();
-        res.send({user : user._id}); 
-    }catch(err){
-        res.status(400).send(err);
-    }
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashPassword,
+  });
+  try {
+    await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-
-
-//add messages here for login,registeration
 // eslint-disable-next-line consistent-return
 router.post('/login', async (req, res) => {
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-    const user = await User.findOne({email:req.body.email});
-    if(!user) return res.status(400).send('Incorrect email/password'); 
 
-    const validPass = await bcrypt.compare(req.body.password,user.password);
-    if(!validPass) return res.status(400).send('Incorrect password/email');
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Incorrect email/password');
 
-    const token = jwt.sign({id: user._id},process.env.AUTH_TOKEN_SECRET);
-    const data = {username : user.name, isPremium : user.isPremium, score : user.score}
-    res.header('auth-token',token).send(token);
-})
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send('Incorrect password/email');
+
+  const token = jwt.sign({ id: user._id }, process.env.AUTH_TOKEN_SECRET);
+  res.header('Auth-Token', token).send(token);
+});
+
 module.exports = router;
